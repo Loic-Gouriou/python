0028f63e0eada592f15082958241efac31cf465a
Loïc <loic.gouriou@edu.itescia.fr>
2020-10-26 11:22:21 +0100

    premier commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from art import *
+
+
+def say_hello(name):
+    print("Hello " + name)
+    print(f"How are you {name} ?")
+
+n = "Bob"
+# say_hello(n)
+h = art("Hello")
+print(h)
+print(text2art("eheh"))
+
+
+
